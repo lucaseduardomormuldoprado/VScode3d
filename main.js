2ecf93a1ac9811544dc7d84d24becd4bc95afc41
lucas <110677140+lucaseduardomormuldoprado@users.noreply.github.com>
2024-05-10 09:30:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-10T00:00:00");
-let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+function calculatempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempofinal = tempoObjetivo - tempoAtual;
+    let segundos = math.floor(tempofinal / 1000);
+    let minutos = math.floor(segundos/60);
+    let horas = math.floor(minutos/60);
+    let dias = math.floor(horas/24);
+    return dias;
+}
+
+contadores[0].textContent = calculatempo(tempoObjetivo);
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
