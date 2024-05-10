22acae54d636de5e7371e9b4c71f20002394a404
lucas <110677140+lucaseduardomormuldoprado@users.noreply.github.com>
2024-05-10 09:04:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-05-10T00:00:00");
+let tempoAtual = new Date();
 
-for (let i=0; i<botoes.length;i++){
-    botoes[i].onclick=function(){
-        for(j=0;j<botoes.length;j++){
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for (j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
