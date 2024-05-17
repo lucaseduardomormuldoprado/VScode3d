153694ce919cfdbe717dfe13709357df376f1dac
lucas <110677140+lucaseduardomormuldoprado@users.noreply.github.com>
2024-05-17 09:30:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,13 +14,26 @@ for (let i=0; i<botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-20T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-20T00:00:00");
+const tempoObjetivo3= new Date("2024-05-20T00:00:00");
+const tempoObjetivo4= new Date("2024-05-20T00:00:00");
 
-function calculaTempo(tempoObjetivo){
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+contadores[0].textContent = alculaTempo(tempos[0]);
+
+function calculaTempo(tempos[0]){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
-    return dias ;
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias+"dias "+horas+"horas "+minutos+"minutos "+segundos+"segundos";
 }
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
